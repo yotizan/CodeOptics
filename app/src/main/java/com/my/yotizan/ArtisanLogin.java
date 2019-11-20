bc8c73e8f9db891708d401705961a95f53626f1e
Sozen Kumora <sozenkumora@gmail.com>
2019-11-20 11:36:29 +0200

    "Marvel created Artisan Login"

---
+++ app/src/main/java/com/my/yotizan/ArtisanLogin.java
@@ -0,0 +1,13 @@
+package com.my.yotizan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ArtisanLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artisan_login);
+    }
+}
